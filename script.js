22c38e735a47167dacd55d40e8202bad59679c0f
navnitanath <nancynath751@gmail.com>
2025-04-11 12:36:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-//complete this code
-class Rectangle {}
+// get set shapes
 
-class Square extends Animal {}
+
+//complete this code
+ class Rectangle {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+    }
+
+    getArea() {
+      return this.width * this.height;
+    }
+  }
+
+  // Square class (inherits from Rectangle)
+  class Square extends Rectangle {
+    constructor(side) {
+      super(side, side); // Pass the same value for width and height
+    }
+
+    getPerimeter() {
+      return 4 * this.width; // Perimeter of a square (4 * side)
+    }
+  }
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
